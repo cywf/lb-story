a2cdd6e67a32ce729649f580673e26bec64a3a50
cywf <kyle.parisher@owasp.org>
2023-03-09 10:50:06 -0400

    initial commit

---
+++ story.py
@@ -0,0 +1,99 @@
+print("Welcome to the interactive story of Lizbeth, a talented programmer from Puerto Rico!\n")
+name = input("What is your name?\n")
+
+print(f"\nNice to meet you, {name}! Let's begin.")
+
+print("\nLizbeth is a young woman from Puerto Rico who is part of Gen Z.")
+print("She studies full-stack development at HolbertonPR and is fluent in both Spanish and English.")
+print("Despite her youth, she is a talented programmer with a passion for technology and innovation.")
+
+print("\nOne day, Lizbeth was working on a project when she received an urgent call from her boss, Carlos.")
+print("Carlos explained that a powerful hacker had breached the company's servers and stolen sensitive information.")
+print("He pleaded with Lizbeth to help, as she was their best hope for stopping the hacker and preventing further damage.")
+
+print("\nLizbeth had a choice to make. What should she do?")
+
+print("Option 1: Agree to help Carlos and dive into the company's servers to stop the hacker.")
+print("Option 2: Refuse to help and focus on her own work instead.")
+
+option = input("What should Lizbeth do? Enter 1 or 2.\n")
+
+if option == "1":
+    print("\nLizbeth was shocked but immediately sprang into action. She knew this was a serious situation that required her full attention and expertise.")
+    print("She quickly gathered her tools and prepared to enter the company's servers.")
+
+    print("\nAs she navigated the maze-like network, Lizbeth could feel the hacker's presence growing stronger.")
+    print("She knew she had to act fast before they could do any more damage.")
+
+    print("\nWith her expert coding skills and quick reflexes, Lizbeth managed to track down the hacker and engage in a tense battle of wits.")
+    print("The hacker was a formidable opponent, using advanced techniques to try to block Lizbeth's every move.")
+
+    print("\nBut Lizbeth was not deterred. She knew she had to stay calm and keep her wits about her.")
+    print("She finally managed to break through the hacker's defenses and gain access to their system.")
+
+    print("\nWith a flurry of keystrokes, Lizbeth quickly set to work neutralizing the hacker's attack and securing the company's data.")
+    print("After a few intense minutes, she emerged victorious.")
+
+    print("\nCarlos was overjoyed and thanked Lizbeth for her quick thinking and skillful work.")
+    print("He marveled at how she had managed to defeat a hacker that had stumped even their best security experts.")
+
+    print("\nAs Lizbeth went back to her work, she noticed a message on her computer screen from a secret admirer named KP.")
+    print("KP had been watching Lizbeth's work and was impressed by her skill and bravery.")
+    print("The message read, 'Lizbeth, you are an amazing programmer. I am in awe of your talent and your courage. Would you like to go out with me sometime?'")
+
+    print("\nLizbeth had a choice to make. What should she do?")
+
+    print("Option 1: Agree to go out with KP and see where it leads.")
+    print("Option 2: Politely decline KP's invitation and focus on her work instead.")
+
+    option2 = input("What should Lizbeth do? Enter 1 or 2.\n")
+
+    if option2 == "1":
+        print("\nLizbeth smiled and replied to KP's message, saying she would love to go out with him.")
+        print("\nKP was overjoyed and sent Lizbeth the details of their date.")
+        print("Lizbeth felt a flutter in her heart as she read the message.")
+
+        print("\nA few days later, Lizbeth met KP at a trendy café in San Juan.")
+        print("They hit it off immediately, talking about their shared love of technology and their dreams for the future.")
+
+        print("\nAs they finished their coffee, KP took Lizbeth's hand and looked into her eyes.")
+        print("He said, 'Lizbeth, I know we just met, but I feel like there's a real connection between us. Will you be my girlfriend?'")
+
+        print("\nLizbeth had a choice to make. What should she do?")
+
+        print("Option 1: Say yes and take a chance on love.")
+        print("Option 2: Say no and focus on her career instead.")
+
+option3 = input("What should Lizbeth do? Enter 1 or 2.\n")
+
+if option3 == "1":
+    print("\nLizbeth felt her heart race as she looked into KP's eyes.")
+    print("She knew she was taking a risk, but something about him made her feel alive.")
+
+    print("\nShe said yes, and KP pulled her into a warm embrace.")
+    print("As they walked out of the café, Lizbeth felt like she was walking on air.")
+
+    print("\nMonths went by, and Lizbeth and KP fell deeper in love with each passing day.")
+    print("They worked on programming projects together, explored Puerto Rico's beautiful beaches, and shared their hopes and fears for the future.")
+
+    print("\nAnd through it all, Lizbeth continued to be a talented and dedicated programmer.")
+    print("She knew that love and career could coexist, and that she was capable of achieving anything she set her mind to.")
+
+    print("\nThanks for joining us in the interactive story of Lizbeth, a talented programmer from Puerto Rico!")
+    print("We hope you enjoyed it, and we wish you all the best in your own adventures.")  
+
+elif option3 == "2":
+    print("\nLizbeth smiled and thanked KP for the invitation.")
+    print("She explained that while she enjoyed spending time with him, her career was her top priority right now.")
+
+    print("\nKP was disappointed but understanding.")
+    print("He thanked Lizbeth for her honesty and told her that he admired her dedication and drive.")
+
+    print("\nAs they parted ways, Lizbeth knew that she had made the right choice.")
+    print("She felt proud of herself for staying true to her passions, and knew that there would be plenty of time for love in the future.")
+
+    print("\nThanks for joining us in the interactive story of Lizbeth, a talented programmer from Puerto Rico!")
+    print("We hope you enjoyed it, and we wish you all the best in your own adventures.") 
+
+else:
+    print("\nSorry, that was an invalid input. Please try again.")
